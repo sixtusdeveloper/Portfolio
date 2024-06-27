0035a81649dec0f3f8f22955a9fabcfb4a9435a1
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-06-28 00:18:35 +0100

    commit on few files

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,11 @@
+// Note: This is the BentoGrid component for the project. It is a grid component that is used to display the grid items in the project. It is used in the Grid component
+// Also install this npm i --save-dev @types/react-lottie
+import React from 'react';
+import { useState } from 'react';
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "@/components/ui/GradientBg";
 import { GlobeDemo } from "./GridGlobe";
+
 
 export const BentoGrid = ({
   className,
@@ -12,7 +17,8 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
@@ -23,21 +29,18 @@ export const BentoGrid = ({
 
 export const BentoGridItem = ({
   className,
+  id,
   title,
   description,
-  id,
   img,
   imgClassName,
   titleClassName,
   spareImg,
-
 }: {
   className?: string;
+  id?: number;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
-  id?: number;
   img?: string;
   imgClassName?: string;
   titleClassName?: string;
@@ -46,86 +49,88 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
       style={{
-        //   you can generate the color from here https://cssgradient.io/
+       
+        //   I generated this color from here https://cssgradient.io/
         background: "rgb(4,7,29)",
         backgroundColor:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-
-        <div className={`${id === 6 && "flex justify-center"} h-full`}>
-          <div className="h-full absolute">
-            {img && (
-                <img 
-                src={img}
-                alt={img}
-                className={cn(imgClassName, 'object-cover, object-center')}
-                />
-            )}
-          </div>
-          
-          <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
-            {spareImg && (
-                <img 
-                src={spareImg}
-                alt={spareImg}
-                className="object-cover, object-center w-full h-full"
-                />
-            )}
-          </div>
-          {id === 6 && (
-            <BackgroundGradientAnimation>
-                <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
-            </BackgroundGradientAnimation>
+      {/* add img divs */}
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center ")}
+            />
           )}
-
-            <div className={cn(
-                titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10')}>
-                    <div className="font-sans font-extralight text-[#C1C2D3] text-sm md:text-xs lg:text-base z-10">
-                        {description}
-                    </div>
-
-                    <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
-                        {title}
-                    </div>
-                
-
-                {id === 2 && <GlobeDemo />}
-                {id === 3 && (
-                    <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-                        <div className="flex flex-col gap-3 lg:gap-8">
-                            {['React.js', 'Next.js', 'TypeScript', 'VueJS', 'JavaScript'].map((item) =>(
-                                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
-                                    {item}
-                                </span>
-                            ))}
-                            <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
-                        </div>
-                        <div className="flex flex-col gap-3 lg:gap-8">
-                           <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
-                            {['MongoDB', 'NodeJS', 'Mysql', 'AWS', 'PHP'].map((item) =>(
-                                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
-                                    {item}
-                                </span>
-                            ))}
-                        </div>
-                        <div className="flex flex-col gap-3 lg:gap-8">
-                            {['TailwindCSS', 'Bootstraps', '{CSS}', '<HTML>', 'Sass'].map((item) =>(
-                                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
-                                    {item}
-                                </span>
-                            ))}
-                            <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
-                           
-                        </div>
-                    </div>
-                )}
-            </div>
         </div>
+        <div
+          className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
+            } `}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className="object-cover object-center w-full h-full"
+            />
+          )}
+        </div>
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+          </BackgroundGradientAnimation>
+        )}
+
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+      
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+            {description}
+          </div>
+    
+          <div
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+          >
+            {title}
+          </div>
+
+          {id === 2 && <GlobeDemo />}
+
+          {/* My tech stecks */}
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                  {['React.js', 'Next.js', 'TypeScript', 'VueJS', 'JavaScript'].map((item) =>(
+                      <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                          {item}
+                      </span>
+                  ))}
+                  <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+                  <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+                  {['MongoDB', 'NodeJS', 'Mysql', 'AWS', 'PHP'].map((item) =>(
+                      <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                          {item}
+                      </span>
+                  ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
