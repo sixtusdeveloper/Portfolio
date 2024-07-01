8ac1167f75c0558093edf202ee080abf06c02508
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-07-01 02:48:11 +0100

    commit on few files

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,9 +7,6 @@ import { socialMedia } from '@/data'
 const Footer = () => {
   return (
     <footer className='w-full pt-10 py-8' id="contact">
-        <div className='w-full absolute left-0 -bottom-72 min-h-96'>
-           <img src="/footer-grid.svg" alt="grid" className='w-full h-full opacity-50'/>
-        </div>
         <div className='flex flex-col items-center'>
             <h1 className='heading lg:max-w-[45vw]'>Ready to take 
             <span className='text-purple'> your digital presence </span> to the next level?
@@ -24,8 +21,8 @@ const Footer = () => {
                 />
             </a>
         </div>
-        <div className='flex lg:mt-48 md:mt-40 mt-45 md:flex-row flex-col justify-between items-center'>
-            <p className="md:text-base text-sm md:font-normal font-light">Copyright&copy; 2024 <span className="text-purple">Sixtusdev</span> | All rights reserved | terms & conditions.</p>
+        <div className='flex lg:mt-40 mt-40 md:flex-row flex-col justify-between items-center'>
+            <p className="md:text-base py-6 text-sm md:font-normal font-light">Copyright&copy; 2024 <span className="text-purple">Sixtusdev</span> | All rights reserved | terms & conditions.</p>
             <div className='flex items-center md:gap-3 gap-6'>
              {socialMedia.map((profile) => (
                 <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
