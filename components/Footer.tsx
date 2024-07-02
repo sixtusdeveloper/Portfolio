d5d65ebe0d5dd0e54b8e8b45ae475801c52da99f
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-07-03 00:00:57 +0100

    Commit on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
             </a>
         </div>
         <div className='flex lg:mt-40 mt-40 md:flex-row flex-col justify-between items-center'>
-            <p className="md:text-base py-6 text-sm md:font-normal font-light">Copyright&copy; 2024 <span className="text-purple">developed by Sixtusdev</span> | All rights reserved | terms & conditions.</p>
+            <p className="md:text-base py-6 text-sm md:font-normal font-light">Copyright&copy; 2024 <span className="text-purple">Developed by Sixtusdev</span> | All rights reserved | terms & conditions.</p>
             <div className='flex items-center md:gap-3 gap-6'>
              {socialMedia.map((profile) => (
                 <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
